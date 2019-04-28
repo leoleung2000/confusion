8315d475a6ad8d79527aa84e642f706963dcfe0f
Leo <leoleung2000@yahoo.com.hk>
2019-04-28 22:50:28 +0800

    Fetch from server

--- src/redux/comments.js
+++ src/redux/comments.js
@@ -1,4 +1,3 @@
-import { COMMENTS } from '../shared/comments';
 import * as ActionTypes from './ActionTypes';
 
 //Remark : THis is reducer , 
@@ -7,15 +6,21 @@ import * as ActionTypes from './ActionTypes';
 // Remember that actions only describe what happened, 
 // but don't describe how the application's state changes.
 
-export const Comments = (state = COMMENTS, action) => {
+export const Comments = (state = { errMess: null, comments:[]}, action) => {
     switch (action.type) {
-        case ActionTypes.ADD_COMMENT:
-            var comment = action.payload;
-            comment.id = state.length;
-            comment.date = new Date().toISOString();
-            console.log("Comment: ", comment);
-            return state.concat(comment);
-        default:
-          return state;
-      }
-};
+      case ActionTypes.ADD_COMMENTS:
+        return {...state, errMess: null, comments: action.payload};
+  
+      case ActionTypes.COMMENTS_FAILED:
+        return {...state, errMess: action.payload};
+  
+      case ActionTypes.ADD_COMMENT:
+          var comment = action.payload;
+          comment.id = state.comments.length;
+          comment.date = new Date().toISOString();
+          return { ...state, comments: state.comments.concat(comment)};
+  
+      default:
+        return state;
+    }
+  };
